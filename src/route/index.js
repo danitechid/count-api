0e963255c4ec3d397d91bf5edeef02ad991dcb53
danzzxcode <102159908+danzzxcode@users.noreply.github.com>
2023-05-11 22:29:08 +0700

    Add files via upload

--- src/route/index.js
+++ src/route/index.js
@@ -4,31 +4,62 @@ const route = router;
 const fs = require('fs');
 
 route.get('/', (req, res) => {
-    res.redirect('/api?url=');
+    res.redirect('/hit?url=visits');
 });
 
-route.get('/api', function(req, res) {
+route.get('/api', (req, res) => {
+    res.redirect('/hit?url=visits');
+});
+
+route.get('/hit', function(req, res) {
     const url = req.query.url
     if (!url) return res.send('Enter URL params')
     
-    const json = fs.readFileSync(__path + '/database/count.json', 'utf-8');
+    const json = fs.readFileSync(__path + '/database/hit/' + url + '.json', 'utf-8');
     const obj = JSON.parse(json);
     
     obj.hit = obj.hit+1;
     
     const newJSON = JSON.stringify(obj);
     
-    fs.writeFileSync(__path + '/database/count.json', newJSON);
+    fs.writeFileSync(__path + '/database/hit/' + url + '.json', newJSON);
     res.send(newJSON);
 });
 
-route.get('*', (req, res) => {
-    res.status(404).json({
-        method : req.method,
-        message : 'cant find spesific endpoint, please make sure you read a documentation',
-        status : false,
-        code : 401,
-    });
+route.get('/hit/visits', function(req, res) {
+    const json = fs.readFileSync(__path + '/database/hit/visits.json', 'utf-8');
+    const obj = JSON.parse(json);
+    
+    obj.hit = obj.hit+1;
+    
+    const newJSON = JSON.stringify(obj);
+    
+    fs.writeFileSync(__path + '/database/hit/visits.json', newJSON);
+    res.send(newJSON);
+});
+
+route.get('/hit/requesttoday', function(req, res) {
+    const json = fs.readFileSync(__path + '/database/hit/requesttoday.json', 'utf-8');
+    const obj = JSON.parse(json);
+    
+    obj.hit = obj.hit+1;
+    
+    const newJSON = JSON.stringify(obj);
+    
+    fs.writeFileSync(__path + '/database/hit/requesttoday.json', newJSON);
+    res.send(newJSON);
+});
+
+route.get('/hit/totalrequest', function(req, res) {
+    const json = fs.readFileSync(__path + '/database/hit/totalrequest.json', 'utf-8');
+    const obj = JSON.parse(json);
+    
+    obj.hit = obj.hit+1;
+    
+    const newJSON = JSON.stringify(obj);
+    
+    fs.writeFileSync(__path + '/database/hit/totalrequest.json', newJSON);
+    res.send(newJSON);
 });
 
 module.exports = route;
