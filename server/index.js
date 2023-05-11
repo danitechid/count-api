8971b9de881fe3cafc43be8f937960a631ccd86f
danzzxcode <ak4nfake196453@gmail.com>
2023-05-11 18:00:48 +0700

    firs commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const route = require('../src/route/index');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(route);
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    try {
+        console.log(`Running on ${port} without you`);
+    } catch (error) {
+        throw error;
+    }
+});
---
+++ src/route/index.js
@@ -0,0 +1,25 @@
+__path = process.cwd()
+const router = require('express').Router();
+const route = router;
+const fs = require('fs');
+
+route.get('/', (req, res) => {
+    res.redirect('/api?url=');
+});
+
+route.get('/api', function(req, res) {
+    const url = req.query.url
+    if (!url) return res.send('Enter URL params')
+    
+    const json = fs.readFileSync(__path + '/database/count.json', 'utf-8');
+    const obj = JSON.parse(json);
+    
+    obj.hit = obj.hit+1;
+    
+    const newJSON = JSON.stringify(obj);
+    
+    fs.writeFileSync(__path + '/database/count.json', newJSON);
+    res.send(newJSON);
+})
+
+module.exports = route;
